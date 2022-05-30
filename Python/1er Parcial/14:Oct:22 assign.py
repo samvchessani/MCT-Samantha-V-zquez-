ae15ac8647425371015428c4853e105f682cc348
samvchessani <104381726+samvchessani@users.noreply.github.com>
2022-05-30 12:09:02 -0500

    Add files via upload

---
+++ Python/1er Parcial/14:Oct:22 assign.py
@@ -0,0 +1,20 @@
+trombones76="hola"
+more=1000000
+classe="hello there"
+
+#math operators
+
+alpha=2
+b3t4=6
+
+suma=alpha+b3t4
+print("la suma jue:", suma)
+
+resta=alpha-b3t4
+print("la resta jue:", resta)
+
+multp=alpha*b3t4
+print("multipliquense:", multp)
+
+div=alpha/b3t4
+print("dividanse:", div)
