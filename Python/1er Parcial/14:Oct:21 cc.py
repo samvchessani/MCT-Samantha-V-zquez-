bf29914a1a4a23798da4a968440ac2aabb4dac27
samvchessani <104381726+samvchessani@users.noreply.github.com>
2022-05-30 12:11:14 -0500

    Add files via upload

---
+++ Python/1er Parcial/14:Oct:21 cc.py
@@ -0,0 +1,41 @@
+#code for the week
+
+mct=6
+mct2=8.5
+mct3=9
+suma=mct+mct2+mct3
+prom_mct=suma/3
+print("El promedio de mct es:", prom_mct)
+
+esp=9
+esp2=9.5
+esp3=10
+suma=esp+esp2+esp3
+prom_esp=suma/3
+print("El promedio de español es:", prom_esp)
+
+math=7.4
+math2=8
+math3=9.6
+suma=math+math2+math3
+prom_math=suma/3
+print("El promedio de matemáticas es:", prom_math)
+
+ss=8.5
+ss2=9.5
+ss3=10
+suma=ss+ss2+ss3
+prom_ss=suma/3
+print("El promedio de Social Studies es:", prom_ss)
+
+LA=6
+LA2=7.6
+LA3=8.5
+suma=LA+LA2+LA3
+prom_LA=suma/3
+print("El promedio de Language Arts es:", prom_LA)
+
+suma=prom_mct+prom_esp+prom_math+prom_ss+prom_LA
+prom_total=suma/5
+print("El promedio total es:", prom_total)
+
