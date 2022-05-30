779046eb7c95c7354bb3b4fb4ce629eb4c5a0e4c
samvchessani <104381726+samvchessani@users.noreply.github.com>
2022-05-30 12:03:11 -0500

    Add files via upload

---
+++ Python/1er Parcial/07:Oct:21:2.py
@@ -0,0 +1,19 @@
+print("aprobados")
+
+
+print(5)
+
+
+mensaje="aprobados"
+mensaje2="reprobados"
+
+print(mensaje)
+print(mensaje2)
+
+mensaje="mas tarea para el 9 virtual"
+
+print(mensaje)
+
+print("este es el mensaje guardado en memoria:",mensaje)
+
+
