5a688da9dfa1332e21879e59144142c0dd9b9152
samvchessani <104381726+samvchessani@users.noreply.github.com>
2022-05-30 12:05:27 -0500

    Add files via upload

---
+++ Python/1er Parcial/07:Oct:21:assign .py
@@ -0,0 +1,46 @@
+name="Samantha"
+print("My name is",name)
+
+grade="9 grade"
+print("Im in",grade)
+
+group="D"
+print("Im in the group of",group)
+
+favoritefood="pasta"
+print("My favorite food is",favoritefood)
+
+random="I like to see Netflix"
+print("Something random is that",random)
+
+
+name="Camila"
+print("My name is",name)
+
+grade="9 grade"
+print("Im in",grade)
+
+group="D"
+print("Im in the group of",group)
+
+favoritefood="carpaccio"
+print("My favorite food is",favoritefood)
+
+random="Im happy that tomorrow is friday"
+print("Something random is that",random)
+
+
+name="Fatima"
+print("My name is",name)
+
+grade="9 grade"
+print("Im in",grade)
+
+group="D"
+print("Im in the group of",group)
+
+favoritefood="sushi"
+print("My favorite food is",favoritefood)
+
+random="I like to play sports"
+print("Something random is that",random)
