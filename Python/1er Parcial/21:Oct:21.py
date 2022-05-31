32601f00dfeb0fbf6eecfe67b9e87b0b4abb46de
samvchessani <104381726+samvchessani@users.noreply.github.com>
2022-05-30 20:39:20 -0500

    Add files via upload

---
+++ Python/1er Parcial/21:Oct:21.py
@@ -0,0 +1,15 @@
+print("How can we act with empathy and positivity when we are online?")
+
+print("Just say what you would say in real life, like in the face of the person")
+
+print("The people involved are the guy, the girl, the friend of the girl and the autorities of the school")
+
+print("The girl share with her friend the letter that a boy wrote to her but the friend share it in social media")
+
+print("The guy is disapointed and the girl is ashamed of what she did")
+
+print("The girl is in problem with the guy because it was something personal, the boy is being transfered to another school")
+
+print("The guy could say those words directly to her, the girl should not send the letter to other person, the friend should not share the letter in social media")
+
+print("The guy could talk to the girl to say what he felt about her, and the girl could appologize and say that it was all the friend s guilt")
